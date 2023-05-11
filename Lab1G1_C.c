2e8214f2fcbe68726b249e48c132828c7fc01d3f
newrohood <131488579+newrohood@users.noreply.github.com>
2023-05-11 12:43:11 -0500

    Primera
    Código creado en clase para prueba del ADC

--- Lab1G1_C.c
+++ Lab1G1_C.c
@@ -1,30 +1,51 @@
-//Primera sección
 #include <xc.h>
 #define _XTAL_FREQ 1000000
-//Segunda sección
-#pragma config FOSC=INTOSC_EC
-#pragma config WDT=OFF
-//Tercera sección
-char a=10;
-int b;
-long c;
-float d;    //double e;
-char f[3];
-int g[2][3];
-long *h;
-//Cuarta sección
-void main(void){ //RD2 rojo  RD1 amarillo  RD0 verde
-  TRISD=0;  //clrf TRISD
-  //TRISD0=0;   //bcf TRISD,0
+#pragma config FOSC=INTOSCIO
+#pragma config WDTEN=OFF
+void Transmitir(unsigned char);
+unsigned char Recibir(void);
+void Transmitir2(char *);
+void main(void){
+  unsigned char BufferR; 
+  TRISC6=1;
+  TRISC7=1;
+  TXSTA=0b00100100;
+  RCSTA=0b10010000;
+  SPBRG=25;
+  BAUDCON=0b00001000;
+  BufferR=RCREG;
+  RCIF=0;
   while(1){
-    LATD=0b00000001;
-    while(RC0==1);
-    __delay_ms(3000);
-    LATD=0b00000010;
-    __delay_ms(3000);
-    LATD=0b00000100;
-    __delay_ms(10000);
-    LATD=0b00000110;
-    __delay_ms(3000);
+    /*BufferR=Recibir();
+    Transmitir2("Usted presiono: ");
+    Transmitir('\t');
+    Transmitir(BufferR);
+    Transmitir('\n');*/
+    Transmitir('H');
+    Transmitir('o');
+    Transmitir('l');
+    Transmitir('a');
+    Transmitir(' ');
+    Transmitir('m');
+    Transmitir('u');
+    Transmitir('n');
+    Transmitir('d');
+    Transmitir('o');
+    Transmitir('\n');
+    __delay_ms(1000);
   }  
 }
+void Transmitir(unsigned char BufferT){
+  while(TRMT==0);
+  TXREG=BufferT;  
+}
+unsigned char Recibir(void){
+  while(RCIF==0);
+  return RCREG;    
+}    
+void Transmitir2(char *BufferT){
+  while(*BufferT!='\0'){
+    Transmitir(*BufferT);
+    BufferT++;
+  }
+}    
